a35a74059383564f3edbe671c6df256923fb185f
DoVietHa-TechTrek <HaDV@talent.apero.vn>
2025-08-07 18:05:33 +0700

    update: using shared preference

--- DoVietHa_BT/app/src/main/java/com/example/dovietha_bt/common/UserInformation.kt
+++ DoVietHa_BT/app/src/main/java/com/example/dovietha_bt/common/UserInformation.kt
@@ -1,6 +1,10 @@
 package com.example.dovietha_bt.common
 
+import android.content.Context
 import android.net.Uri
+import androidx.core.net.toUri
+import com.example.dovietha_bt.UserPreferences
+import com.example.dovietha_bt.database.entity.User
 
 object UserInformation {
     var username: String = ""
@@ -9,4 +13,11 @@ object UserInformation {
     var university: String? = null
     var desc: String? = null
     var image: Uri? = null
+    fun updateData(user: User){
+        username = user.userName
+        phone = user.phoneNumber
+        university = user.university
+        desc = user.desc
+        image = user.avatarUrl.takeIf { it?.isNotBlank() == true }?.toUri()
+    }
 }
